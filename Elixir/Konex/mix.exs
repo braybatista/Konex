4e03750ec61c6638afeaa9a17e68599af0dc0be5
Brayan batista zuniga <bbatist@bancolombia.com.co>
2024-02-26 11:44:16 -0500

    fix: fixing working directory using mix lock at parent 9

--- Elixir/Konex/mix.exs
+++ Elixir/Konex/mix.exs
@@ -1,1 +1,30 @@
-IO.puts("Hello, World!")
+defmodule ReactiveCommons.MixProject do
+  use Mix.Project
+
+  @version "0.8.4"
+
+  def project do
+    [
+      app: "Hello world app",
+      version: @version,
+      deps: deps(),
+    ]
+  end
+
+  defp hello(_) do
+    Mix.shell().info("Hello world")
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      {:poison, "~> 5.0"},
+      {:amqp, "~> 3.2"},
+      {:uuid, "~> 1.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:telemetry, "~> 1.2"},
+      {:mock, "~> 0.3", only: :test}
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
