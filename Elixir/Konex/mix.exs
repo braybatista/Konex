89fde884446e1c0d0899ed91de385c0516feae3e
Brayan batista zuniga <bbatist@bancolombia.com.co>
2024-02-26 14:43:00 -0500

    fix: test publish to hex, set working directory, release version, testing publish and error with name, removing name

--- Elixir/Konex/mix.exs
+++ Elixir/Konex/mix.exs
@@ -5,7 +5,7 @@ defmodule ReactiveCommons.MixProject do
 
   def project do
     [
-      app: :Konex_world_app,
+      app: :Konex,
       version: @version,
       deps: deps(),
       description: description(),
@@ -39,7 +39,7 @@ defmodule ReactiveCommons.MixProject do
   defp package() do
     [
       # This option is only needed when you don't want to use the OTP application name
-      name: "Konex_world_app",
+      #name: "Konex",
       files: ["test", "mix.exs", ".formatter.exs", "mix.lock"],
       maintainers: ["Brayan Batista Zuniga"],
       licenses: ["Apache-2.0"],
