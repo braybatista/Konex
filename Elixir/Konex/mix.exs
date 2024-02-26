4a5243f617559ce014d0c3e142f4cfd68c60a218
Brayan batista zuniga <bbatist@bancolombia.com.co>
2024-02-26 14:21:05 -0500

    feat: test publich to hex

--- Elixir/Konex/mix.exs
+++ Elixir/Konex/mix.exs
@@ -1,18 +1,23 @@
 defmodule ReactiveCommons.MixProject do
   use Mix.Project
 
-  @version "0.0.1"
+  @version "1.0.0"
 
   def project do
     [
       app: :Hello_world_app,
       version: @version,
       deps: deps(),
+      description: description(),
     ]
   end
 
   defp hello(_) do
     Mix.shell().info("Hello world")
+  end
+
+  defp description() do
+    "Domain driven async abstractions like Domain Event Bus, Event subscriptions/emit, Async Command handling and Async Req/Reply."
   end
 
   # Run "mix help deps" to learn about dependencies.
@@ -25,7 +30,18 @@ defmodule ReactiveCommons.MixProject do
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:telemetry, "~> 1.2"},
       {:mock, "~> 0.3", only: :test}
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # {:dep_from_git, git: "https://github.com/braybatista/Konex.git", tag: "1.0.0"}
+    ]
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "Konex",
+      files: ["test", "mix.exs", "formatter.exs", "mix.lock"],
+      maintainers: ["Brayan Batista Zuniga"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/braybatista/Konex"}
     ]
   end
 end
